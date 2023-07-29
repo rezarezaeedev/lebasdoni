a66a5c8701f27e7c4827ecbd6103c805448c8da1
Reza Rezaee <rezarezaee.commercial@gmail.com>
2023-07-29 15:54:51 +0330

    make a urls file for products app and put app label in setting.py INSTALLED_APPS apps list.

--- LebasdonRoot/settings.py
+++ LebasdonRoot/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Installed Apps
+    'rest_framework',
+
+    # My Apps
+    'Products.apps.ProductsConfig',
 ]
 
 MIDDLEWARE = [
