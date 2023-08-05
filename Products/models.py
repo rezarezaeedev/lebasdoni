4253825270679835105a7ba83287cb94612e0894
Reza Rezaee <rezarezaee.commercial@gmail.com>
2023-08-05 12:23:53 +0330

    add sex model and add size field

--- Products/models.py
+++ Products/models.py
@@ -1,16 +1,23 @@
 from django.db import models
 
 
+class Sex(models.Model):
+	sex = models.BooleanField(help_text="True meant Man's sex and False is Woman")
+
+	def __str__(self):
+		return 'Man' if self.sex else 'Woman'
+
+
 class ProductInfo(models.Model):
 	name = models.CharField(max_length=50)
 	color = models.CharField(max_length=20)
 	price = models.FloatField()
-	# sex = FK to sex model
+	sex = models.ForeignKey('Sex', on_delete=models.CASCADE)
+	size = models.SmallIntegerField()
 	# material = FK 2 Material 
-	# size = FK to size model
 
-	def str(self):
-		return f'{self.name} {self.color} {self.price}'
+	def __str__(self):
+		return f'{self.name} {self.color} {self.price}$'
 
 
 class Product(models.Model):
@@ -20,5 +27,5 @@ class Product(models.Model):
 	imported_time = models.DateTimeField(auto_now_add=True, editable=False)
 	exported_time = models.DateTimeField(null=True, blank=True, editable=False)
 
-	def str(self):
+	def __str__(self):
 		return f'Availablity:{self.is_available} ,Reservilty:{self.is_reserved}'
